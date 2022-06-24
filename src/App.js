7510eeed59e2e3d4593a07c1299a5573c1ff9d59
enkog <nkirukaogu@gmail.com>
2022-06-24 10:41:14 +0100

    Add page layout and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{ width: { xl: '1488px' }, m:"auto" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
