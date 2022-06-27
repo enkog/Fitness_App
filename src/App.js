5d6ed7b2aca473a8b9cd56a69466daf937573a7a
enkog <nkirukaogu@gmail.com>
2022-06-27 10:45:16 +0100

    Add layout for the search section

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Box width="400px" sx={{ width: { xl: '1488px' }, m:"auto" }}>
+    <Box width="400px" sx={{ width: { xl: '1488px' }, m: "auto" }}>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button, Stack, Typography, TextField } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="2px">
+      <Typography fontWeight={700} sx={{fontSize: {lg: '44px', xs: '30px'}}} mb='50px' textAlign='center'>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+      <Box position='relative' mb='72px'>
+        <TextField sx={{
+          input: {
+            border: 'none',
+            borderRadius: '4px', 
+            fontWeight: '700'
+          },
+          width: {lg: '800px', xs: '350px'},
+          backgroundColor: '#fff',
+          borderRadius: '40px'
+          }} 
+          height='76px' value='' placeholder='Search Exercises' type='text' onChange={(e) => {}} />
+          <Button className='search-btn' sx={{
+            backgroundColor: '#ff2625', 
+            color: '#fff', 
+            textTransform: 'none', 
+            width: {lg: '175px', xs: '80px'}, fontSize: {lg: '20px', xs: '14px'}, height: '56px',
+            position: 'absolute',
+            right: '0'
+          }}>Search</Button>
+      </Box>
+    </Stack>
   )
 }
 
